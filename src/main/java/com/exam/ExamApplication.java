d44c6b2a421e2bbfa148b49902e144909071ceb9
maxthunderstorm <dayalanand0813@gmail.com>
2025-08-09 10:41:48 +0530

    Update ExamApplication.java

--- src/main/java/com/exam/ExamApplication.java
+++ src/main/java/com/exam/ExamApplication.java
@@ -8,6 +8,13 @@ public class ExamApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExamApplication.class, args);
+         int sum = 0, count = 0; // variables to store sum and count
+
+        for (int i = 1; i <= 10; i++) { // loop from 1 to 10
+            if (i % 2 == 0) {           // check if number is even
+                System.out.println("Even: " + i); // print even number
+                sum += i;               // add to sum
+                count++;      
 	}
 
 }
